56e3cdf91afc38345a6d9061f973c5015e152108
hiattj19 <hiattj19@gmail.com>
2025-02-21 23:26:27 -0700

    Created functionality for the pdfExtractor

--- src/PdFextractor/Pdfextractor.jsx
+++ src/PdFextractor/Pdfextractor.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './Pdfextractor.css';
 
 import { BuiltScript, ScriptBuilder } from './scriptBuilder';
@@ -9,10 +9,11 @@ export function PDFextractor() {
   const [selectedOption, setSelectedOption] = useState('');
   const fileInputRef = useRef(null);
   const optionInputRef = useRef(null);
+  const [test, setTest] = useState('')
 
   const handleFileInput = () => {
     if (fileInputRef.current && fileInputRef.current.files.length > 0){
-      setSelectedFile(fileInputRef.current.files[0]);
+      setSelectedFile(fileInputRef.current.files[0].name);
     }
     else {
       setSelectedFile(null);
@@ -27,14 +28,10 @@ export function PDFextractor() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const formInfo = new FormData();
-    formInfo.append('file',selectedFile.name);
-    formInfo.append('outType',selectedOption);
-
-    formInfo.forEach(element => {
-      console.log(element);
-    });
-    //const test = new ScriptBuilder(formInfo['file'],formInfo['outType']);
+    new ScriptBuilder(selectedFile, selectedOption)
+    //This will be a 3rd party call to a python script I have already written.
+    let testing =  localStorage.getItem("textString");
+    setTest(JSON.parse(testing));
   };
   return (
     <main>
@@ -44,20 +41,24 @@ export function PDFextractor() {
                   <div>
                     <label className="extractor" htmlFor="pdfFile">Choose PDF to extract text from</label>
                     <input ref={fileInputRef} type="file" id="pdfFile" onChange={handleFileInput}/>
-                    {selectedFile && <p>Selected File: {selectedFile.name}</p>}
+                    {selectedFile && <p>Selected File: {selectedFile}</p>}
                   </div>
                   <div>
                     <label className="fileTypeSelector" htmlFor="fileType">What kind of file would you like text added to?</label>
                     <select ref={optionInputRef} value={selectedOption} id="fileType" name="fileType" onChange={handleSelector}>
                         <option>Select a output file type</option>
-                        <option>.txt</option>
-                        <option>Searchable PDF</option>
-                        <option>HTML (hOCR)</option>
-                        <option>.tsv</option>
+                        <option>txt</option>
+                        <option>pdf</option>
+                        <option>html</option>
+                        <option>tsv</option>
                     </select>
+                    <p>The pdf option returns a searchable pdf and the html returns a hOCR file.</p>
                     </div>
                     <input type='submit' value={message} className="extractor" id="extractInput"/>
                 </form>
+                <div>
+                  <p>{test}</p>
+                </div>
             </section>
     </main>
   );
--- src/PdFextractor/ScriptBuilder.js
+++ src/PdFextractor/ScriptBuilder.js
@@ -4,23 +4,45 @@
      OutputFilePath: 'outputfile',
      scriptName: 'scriptname',
 };
+// let dataFile;
+// let data;
 
 class ScriptBuilder {
     scriptName = "Pdf-splitter.py";
-    constructor(InputFilePath,OutputFilePath) {
-        this.buildScript(this.scriptName,InputFilePath,OutputFilePath);
-        return this.runScript(JSON.parse(localStorage.getItem('myKey')));
-    }
+    // dataFile = localStorage.getItem("myFile");
+    constructor(input, output) {
+        let outputPath = input.slice(0,-3) + output;
+        let newScript = this.buildScript(this.scriptName,input,outputPath);
+        this.runScript(newScript);
+        // if (dataFile){
+        //     try {
+        //         data = JSON.parse(dataFile);
+        //         let inputPath = data.input
+        //         let outputPath = inputPath + data.outputPath;
+        //         let newScript = this.buildScript(this.scriptName,inputPath,outputPath);
+        //         this.runScript(newScript);
+        //     }
+        //     catch (error){
+        //         console.error("Error parsing: ", error);
+        //     }
+            
+        // }
+        // else {
+        //     console.log("No data");
+        }
 
+    removeLastChars(){
+        setSelectedFile(selectedFile.slice(0,-3));
+      }
     buildScript(scriptName,inPath,outPath){
-        BuiltScript = {scriptLanguage: "Python", InputFilePath: inPath, OutputFilePath: outPath, scriptName: scriptName};
-        localStorage.setItem('myKey',JSON.stringify(BuiltScript));
+        BuiltScript = {scriptLanguage: "Python", InputFilePath: inPath, OutputFilePath:outPath, scriptName: scriptName};
+        return BuiltScript;
     }
 
     runScript(command) {
         //Run back end service that runs the pdf-splitter script
-        console.log(command);
+        console.log(command.OutputFilePath);
+        localStorage.setItem("textString",JSON.stringify(command.OutputFilePath));
     }
 }
-
 export {BuiltScript,ScriptBuilder}
