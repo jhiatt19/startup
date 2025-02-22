f5fe553e70c4e14c9bc9cede7509c15dd2ca9cf8
hiattj19 <hiattj19@gmail.com>
2025-02-22 13:04:02 -0700

    Fixed CSS signup page

--- src/SignUpPage/SignUpPage.jsx
+++ src/SignUpPage/SignUpPage.jsx
@@ -6,14 +6,16 @@ export function SignUpPage() {
     <main>
         <section id="sign up">
             <form action="/Home">
-                <label id="uname" for="username">Username</label><br/>
+                <label id="uname" htmlFor="username">Username</label><br/>
                 <input type="text" name="username" placeholder="Username"/><br/>
                 <label htmlFor="password">Password</label><br/>
                 <input type="password" name="password" placeholder="Password"/><br/>
                 <label htmlFor="email">Email (Optional)</label><br/>
                 <input type="email" name="email" placeholder="Enter your email"/><br/>
-                <button type="reset">Reset</button>
-                <button type="Submit">Submit</button>
+                <div className="signUpButtons">
+                  <button type="reset" id='signButton'>Reset</button>
+                  <button type="submit" id='signButton'>Submit</button>
+                </div>
             </form>
         </section>
     </main>
