6136a1110727940d7929224d288c05e6cf52b769
hiattj19 <hiattj19@gmail.com>
2025-03-19 14:40:27 -0600

    set up my npm inside my service folder

--- service/index.js
+++ service/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const bycrypt = require('bycriptjs');
+const uuid = require('uuid');
 const app = express();
+
+const authCookieName = 'token';
+
+let users = [];
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.static('public'));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+var apiRouter = express.Router();
+
+app.use('/api', apiRouter);
+
