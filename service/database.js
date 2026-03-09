3566a110199dc4266ebe272639fe4c2304578d27
jhiatt19 <hiattj19@gmail.com>
2026-03-09 01:01:32 -0600

    Lets try and see if this updates things

--- service/database.js
+++ service/database.js
@@ -1,80 +1,82 @@
-import { MongoClient } from 'mongodb';
-import config from './dbConfig.json' with {type: 'json'};
+import { MongoClient } from "mongodb";
+import "dotenv/config";
 
-const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOSTNAME}`;
 const client = new MongoClient(url);
-const db = client.db('Won_Stop');
-const userCollection = db.collection('users');
-const authCollection = db.collection('auth');
+const db = client.db("Won_Stop");
+const userCollection = db.collection("users");
+const authCollection = db.collection("auth");
 
 (async function testConnection() {
-    try {
-        await db.command({ping: 1});
-        console.log('Connected to database');
-    } catch (ex) {
-        console.log(`Unable to connect to database with ${url} because ${ex.message}`);
-        process.exit(1);
-    }
+  try {
+    await db.command({ ping: 1 });
+    console.log("Connected to database");
+  } catch (ex) {
+    console.log(
+      `Unable to connect to database with ${url} because ${ex.message}`,
+    );
+    process.exit(1);
+  }
 })();
 
-function getUser(user){
-    return userCollection.findOne({username: user});
+function getUser(user) {
+  return userCollection.findOne({ username: user });
 }
 
-function getUserByToken(token){
-    return authCollection.findOne({auth: token});
+function getUserByToken(token) {
+  return authCollection.findOne({ auth: token });
 }
 
 async function addUser(user) {
-    await userCollection.insertOne(user);
+  await userCollection.insertOne(user);
 }
 
 async function updateUser(user) {
-    await userCollection.updateOne({username: user.username}, {$set: user});
+  await userCollection.updateOne({ username: user.username }, { $set: user });
 }
 
-async function addTask(task,user) {
-    console.log(user.tasks);
-    user.tasks[(task.taskID)] = task;
-    await userCollection.updateOne({username: user.username}, {$set: user});
+async function addTask(task, user) {
+  console.log(user.tasks);
+  user.tasks[task.taskID] = task;
+  await userCollection.updateOne({ username: user.username }, { $set: user });
 }
 
-async function deleteTask(user,task){
-    console.log(task);
-    task.forEach(id => {
-        delete user.tasks[id];
-    });
-    await userCollection.updateOne({username: user.username},{$set:user});
+async function deleteTask(user, task) {
+  console.log(task);
+  task.forEach((id) => {
+    delete user.tasks[id];
+  });
+  await userCollection.updateOne({ username: user.username }, { $set: user });
 }
 
-async function getTasks(username){
-    const user = await getUser(username);
-    console.log(user);
-    return user.tasks;
+async function getTasks(username) {
+  const user = await getUser(username);
+  console.log(user);
+  return user.tasks;
 }
 
-async function addAuth(auth,username){
-    await authCollection.insertOne({username:username, auth:auth});
+async function addAuth(auth, username) {
+  await authCollection.insertOne({ username: username, auth: auth });
 }
 
-function getAuthCode(token){
-    return authCollection.findOne({auth: token});
+function getAuthCode(token) {
+  return authCollection.findOne({ auth: token });
 }
 
-async function deleteAuth(token){
-    const username = await getUserByToken(token);
-    await authCollection.deleteMany({username:username});
+async function deleteAuth(token) {
+  const username = await getUserByToken(token);
+  await authCollection.deleteMany({ username: username });
 }
 
 export {
-    getUser,
-    getUserByToken,
-    addUser,
-    addTask,
-    updateUser,
-    addAuth,
-    getAuthCode,
-    deleteAuth,
-    deleteTask,
-    getTasks
-}
+  getUser,
+  getUserByToken,
+  addUser,
+  addTask,
+  updateUser,
+  addAuth,
+  getAuthCode,
+  deleteAuth,
+  deleteTask,
+  getTasks,
+};
--- src/app.jsx
+++ src/app.jsx
@@ -1,137 +1,202 @@
-import React, { useCallback, useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './all.css';
-import {nanoid} from 'nanoid';
+import React, { useCallback, useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./all.css";
+import { nanoid } from "nanoid";
 
-import { BrowserRouter, NavLink, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
-import { Home } from './Home/home';
-import { Alarms } from './Alarms/Alarms';
-import { Calendar } from './Calendar/Calendar';
-import { Login } from './Login/login';
-import { PDFextractor } from './PdFextractor/Pdfextractor';
-import { ProductivityCalendar } from './ProductivityCalendar/ProductivityCalendar';
-import { SignUpPage } from './SignUpPage/SignUpPage';
-import { URLholder } from './URLholder/URLholder';
+import {
+  BrowserRouter,
+  NavLink,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import { Home } from "./Home/home";
+import { Alarms } from "./Alarms/Alarms";
+import { Calendar } from "./Calendar/Calendar";
+import { Login } from "./Login/login";
+import { PDFextractor } from "./PdFextractor/Pdfextractor";
+import { ProductivityCalendar } from "./ProductivityCalendar/ProductivityCalendar";
+import { SignUpPage } from "./SignUpPage/SignUpPage";
+import { URLholder } from "./URLholder/URLholder";
 
-function NavigationBar(){
-    return (
-        <nav>
-            <ul>
-                <li><NavLink to='/home'>Home</NavLink></li>
-                <li><NavLink to='/PDFextractor'>PDF Extractor</NavLink></li>
-                <li><NavLink to='/ProductivityCalendar'>Productivity Calendar</NavLink></li>
-                <li><NavLink to='/Calendar'>Calendar</NavLink></li>
-                <li><NavLink to='/Alarms'>Alarms</NavLink></li>
-                <li><NavLink to='/URLholder'>URL holder</NavLink></li>
-            </ul>
-        </nav>
-    )
-};
+function NavigationBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/home">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/PDFextractor">PDF Extractor</NavLink>
+        </li>
+        <li>
+          <NavLink to="/ProductivityCalendar">Productivity Calendar</NavLink>
+        </li>
+        <li>
+          <NavLink to="/Calendar">Calendar</NavLink>
+        </li>
+        <li>
+          <NavLink to="/Alarms">Alarms</NavLink>
+        </li>
+        <li>
+          <NavLink to="/URLholder">URL holder</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
-export default function App(){
-    const [authState,setAuthState] = useState("Not Authenticated");
-    const [username,setUsername] = useState('');
-    const [buttonText,setButtonText] = useState("Continue as Guest");
-    const navigate = useNavigate();
-    
-    const handleLogOut = useCallback(async () => {
-        try {
-            const response = await fetch('/api/auth/logout', {method: 'delete'});
-            if (response?.status === 200) {
-                setAuthState("Not Authenticated");
-                setButtonText("Continue as Guest");
-                setUsername('');
-                navigate("/");
-            } else {
-                const body = await response.json();
-                console.error(`Logout Error: ${body.msg}`);
-            }
-        } catch (error) {
-            console.error('Logout failed:', error);
-        }
-        
-    }, [navigate]);
+export default function App() {
+  const [authState, setAuthState] = useState("Not Authenticated");
+  const [username, setUsername] = useState("");
+  const [buttonText, setButtonText] = useState("Continue as Guest");
+  const navigate = useNavigate();
 
-    const handleHome = () => {
-        if (authState === "Authenticated"){
-          navigate("/Home");
-        }
-        else {
-          navigate('/');
-        }
-      };
-    
-    const handleGuest = useCallback(async () => {
-        if (username === ''){
-            const guestUser = `Guest-${nanoid(10)}`;
-            try {
-                const response = await fetch('/api/auth/create', {
-                    method: 'post',
-                    body: JSON.stringify({username: guestUser, password: "guest"}),
-                    headers: {
-                        'Content-type': 'application/json',
-                    }
-                });
+  const handleLogOut = useCallback(async () => {
+    try {
+      const response = await fetch("/api/auth/logout", { method: "delete" });
+      if (response?.status === 200) {
+        setAuthState("Not Authenticated");
+        setButtonText("Continue as Guest");
+        setUsername("");
+        navigate("/");
+      } else {
+        const body = await response.json();
+        console.error(`Logout Error: ${body.msg}`);
+      }
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  }, [navigate]);
 
-            if (response?.status === 200){
-                const plaintextResponse = await response.json();
-                setAuthState(plaintextResponse.authState);
-                setUsername(plaintextResponse.username);
-                setButtonText("Welcome Guest!");
-                navigate("/home");
-            } else {
-                const body = await response.json();
-                console.error(`Guest Error: ${body.msg}`);
-            }
-        } catch (error) {
-            console.error('Guest creation failed:', error);
-        }
+  const handleHome = () => {
+    if (authState === "Authenticated") {
+      navigate("/Home");
+    } else {
+      navigate("/");
+    }
+  };
+
+  const handleGuest = useCallback(async () => {
+    if (username === "") {
+      const guestUser = `Guest-${nanoid(10)}`;
+      try {
+        const response = await fetch("/api/auth/create", {
+          method: "post",
+          body: JSON.stringify({ username: guestUser, password: "guest" }),
+          headers: {
+            "Content-type": "application/json",
+          },
+        });
+
+        if (response?.status === 200) {
+          const plaintextResponse = await response.json();
+          setAuthState(plaintextResponse.authState);
+          setUsername(plaintextResponse.username);
+          setButtonText("Welcome Guest!");
+          navigate("/home");
         } else {
-            handleHome;
+          const body = await response.json();
+          console.error(`Guest Error: ${body.msg}`);
         }
-    }, [navigate, username]);
+      } catch (error) {
+        console.error("Guest creation failed:", error);
+      }
+    } else {
+      handleHome();
+    }
+  }, [navigate, username]);
 
-    useEffect(() => {
-        if (authState === 'Not Authenticated'){
-            navigate('/');
-        }
-    },[authState]);
+  useEffect(() => {
+    if (authState === "Not Authenticated") {
+      navigate("/");
+    }
+  }, [authState]);
 
-    return ( 
-        <div>
-        <header>
-            <NavLink to='./Home'><form action='./Home'>
-                <button id="profile" onClick={() => handleGuest()}>{buttonText}</button>
-            </form></NavLink>
-            <h1><a id="homeNav" onClick={handleHome}>Won Stop</a></h1>
-            <NavLink to='./'>
-            <form>
-                <button id="logOut" onClick={() => handleLogOut()}>Log out</button>
-            </form>
-            </NavLink> <br/>
-        </header>
-        {authState === "Authenticated" && <NavigationBar />}
-        <main>
-            <Routes>
-                <Route path='/' element={<Login setAuthState={setAuthState} setUsername={setUsername} username={username} setButtonText={setButtonText}/>} exact />
-                <Route path='/Home' element={<Home authState={authState} setButtonText={setButtonText}/>} />
-                <Route path='/PDFextractor' element={<PDFextractor />} />
-                <Route path='/ProductivityCalendar' element={<ProductivityCalendar username={username} authState={authState}/>} />
-                <Route path='/Calendar' element={<Calendar />} />
-                <Route path='/Alarms' element={<Alarms />} />
-                <Route path='/URLholder' element={<URLholder />} />
-                <Route path='/SignUpPage' element={<SignUpPage setAuthState={setAuthState} setButtonText={setButtonText} setUsername={setUsername} username={username}/>} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
-        </main>
-        <footer>
-            <span id="authorName">Author Name(s): Jordan Hiatt</span>
-            <NavLink id="github"to="https://github.com/jhiatt19/startup">Github</NavLink>
-        </footer>
+  return (
+    <div>
+      <header>
+        <NavLink to="./Home">
+          <form action="./Home">
+            <button id="profile" onClick={() => handleGuest()}>
+              {buttonText}
+            </button>
+          </form>
+        </NavLink>
+        <h1>
+          <a id="homeNav" onClick={handleHome}>
+            Won Stop
+          </a>
+        </h1>
+        <NavLink to="./">
+          <form>
+            <button id="logOut" onClick={() => handleLogOut()}>
+              Log out
+            </button>
+          </form>
+        </NavLink>{" "}
+        <br />
+      </header>
+      {authState === "Authenticated" && <NavigationBar />}
+      <main>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Login
+                setAuthState={setAuthState}
+                setUsername={setUsername}
+                username={username}
+                setButtonText={setButtonText}
+              />
+            }
+            exact
+          />
+          <Route
+            path="/Home"
+            element={
+              <Home authState={authState} setButtonText={setButtonText} />
+            }
+          />
+          <Route path="/PDFextractor" element={<PDFextractor />} />
+          <Route
+            path="/ProductivityCalendar"
+            element={
+              <ProductivityCalendar username={username} authState={authState} />
+            }
+          />
+          <Route path="/Calendar" element={<Calendar />} />
+          <Route path="/Alarms" element={<Alarms />} />
+          <Route path="/URLholder" element={<URLholder />} />
+          <Route
+            path="/SignUpPage"
+            element={
+              <SignUpPage
+                setAuthState={setAuthState}
+                setButtonText={setButtonText}
+                setUsername={setUsername}
+                username={username}
+              />
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <footer>
+        <span id="authorName">Author Name(s): Jordan Hiatt</span>
+        <NavLink id="github" to="https://github.com/jhiatt19/startup">
+          Github
+        </NavLink>
+      </footer>
     </div>
-    )
+  );
 }
 
 function NotFound() {
-    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
-  }
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      404: Return to sender. Address unknown.
+    </main>
+  );
+}
